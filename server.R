24eb561ab5197a8e5bbb0f976dbdf903e45bdaa4
vatsad7 <31946885+vatsad7@users.noreply.github.com>
2018-12-22 17:03:32 +0530

    Update server.R

--- server.R
+++ server.R
@@ -11,9 +11,8 @@ library(shiny)
 library(igraph)
 library(ggraph)
 library(ggplot2)
-
-
-
+library(udpipe)
+library(stringr)
 
 options(shiny.maxRequestSize = 40*1024^2)
 # Define server logic required to draw a histogram
@@ -24,45 +23,84 @@ shinyServer(function(input, output,session) {
   #output$inputfile <- renderPrint({str(input$file)})
   #output$checkbox <- renderPrint({ input$checkGroup })
   
-  textfile <- reactive({
-    if (is.null(input$file1)) {return(NULL)}
+  text_file <- reactive({
+    if (is.null(input$file)) {return(NULL)}
     else {
-      Document <- readLines(input$file1$datapath)
-      corpus = readLines(Document)
-      corpus  =  str_replace_all(corpus, "<.*?>", "")
-      corpus
+      Document <- readLines(input$file$datapath)
+      
+      corpus  =  str_replace_all(Document, "<.*?>", "")
+            return(corpus)
       }
   })
+  model <- reactive({
+    x <- udpipe_download_model(input$select, model_dir = tempdir())
+    return(x$file_model)
+  })
   
-  model_trained <- reactive({
-    english_model = udpipe_load_model(input$file2$datapath)  # file_model only needed
-    # now annotate text dataset using ud_model above
-    # system.time({   # ~ depends on corpus size
-    x <- udpipe_annotate(english_model, x = textfile())#%>% as.data.frame() %>% head()
-    x <- as.data.frame(x)
-    x
+  
+  english_model <- reactive  ({
+    english_model <- udpipe_load_model(model())  # file_model only needed
+    return(english_model)
+     } )
+  
+  annotation_function <- reactive({
+    
+    udp <- udpipe_annotate(english_model(), text_file())#%>% as.data.frame() %>% head()
+    udp <- as.data.frame(udp)
+    return(udp)
     })
   
-  
-  
   output$plot1  <- renderPlot ({
-      text_colloc <- keywords_collocation(x = model_trained(),   # try ?keywords_collocation
-                                          term = "token",group = c("doc_id", "paragraph_id", "sentence_id"),
-                                          ngram_max = 4) 
-      text_cooc <- cooccurrence(# try `?cooccurrence` for parm options
-        x = subset(model_trained(), upos %in% c("NOUN", "ADJ")), 
+        text_cooc <- cooccurrence(# try `?cooccurrence` for parm options
+        x = subset(annotation_function(), upos %in% c(input$checkGroup)), 
         term = "lemma", 
         group = c("doc_id", "paragraph_id", "sentence_id"))
       
       wordnetwork <- head(text_cooc, 30)
       wordnetwork <- igraph::graph_from_data_frame(wordnetwork) # needs edgelist in first 2 colms.
-      cooccurence_graph <ggraph(wordnetwork, layout = "fr") +  
+      
+      ggraph(wordnetwork, layout = "fr") +  
         geom_edge_link(aes(width = cooc, edge_alpha = cooc), edge_colour = "orange") +  
         geom_node_text(aes(label = name), col = "darkgreen", size = 4) +
         theme_graph(base_family = "Arial Narrow") +  
         theme(legend.position = "none") +
         labs(title = "Cooccurrences within 3 words distance")  
-      cooccurence_graph
-    
-    })
-    })
+          })
+  
+  output$wc1 <-  renderPlot({
+    if(is.null(input$file)){return(NULL)}
+    else{
+      all_adjectives = annotation_function() %>% subset(., upos %in% "ADJ") 
+      top_adjectives = txt_freq(all_adjectives$lemma)  # txt_freq() calcs noun freqs in desc order
+      wordcloud(top_adjectives$key,top_adjectives$freq, min.freq = 3,colors = 1:10 )
+    }})
+  output$wc2 <-  renderPlot({
+    if(is.null(input$file)){return(NULL)}
+    else{
+      all_nouns = annotation_function() %>% subset(., upos %in% "NOUN") 
+      top_nouns = txt_freq(all_nouns$lemma)  # txt_freq() calcs noun freqs in desc order
+      wordcloud(top_nouns$key,top_nouns$freq, min.freq = 3,colors = 1:10 )
+    }})
+  
+  output$wc3 <-  renderPlot({
+    if(is.null(input$file)){return(NULL)}
+    else{
+      all_proper_noun = annotation_function() %>% subset(., upos %in% "PROPN") 
+      top_proper_nouns = txt_freq(all_proper_noun$lemma)  # txt_freq() calcs noun freqs in desc order
+      wordcloud(top_proper_nouns$key,top_proper_nouns$freq, min.freq = 3,colors = 1:10 )
+    }})
+  output$wc4 <-  renderPlot({
+    if(is.null(input$file)){return(NULL)}
+    else{
+      all_adverbs = annotation_function() %>% subset(., upos %in% "ADV") 
+      top_adverbs = txt_freq(all_adverbs$lemma)  # txt_freq() calcs noun freqs in desc order
+      wordcloud(top_adverbs$key,top_adverbs$freq, min.freq = 3,colors = 1:10 )
+    }})
+  output$wc5 <-  renderPlot({
+    if(is.null(input$file)){return(NULL)}
+    else{
+      all_verbs = annotation_function() %>% subset(., upos %in% "VERB") 
+      top_verbs = txt_freq(all_verbs$lemma)  # txt_freq() calcs noun freqs in desc order
+      wordcloud(top_verbs$key,top_verbs$freq, min.freq = 3,colors = 1:10 )
+    }})
+  })
